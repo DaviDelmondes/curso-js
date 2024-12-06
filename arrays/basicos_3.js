e12a962d3a9a38689752f5ea823bc543eda1c892
DaviDelmondes <Davidnascimentomederos40@gmail.com>
2024-12-05 21:56:17 -0300

    comit 05 12 2024

---
+++ arrays/basicos_3.js
@@ -0,0 +1,13 @@
+const numeros = [7 , 8, 9]
+
+console.log(typeof numeros)
+
+numeros[3] = 10
+numeros.push(11)
+numeros.push(12,13,14)
+
+console.log(numeros)
+console.log(numeros.length)
+
+// todos arrays são objeto
+// quandos nois tem objeto nois usa . para acessa funçoes ou nois usa . para o atributo ou valor que aqui é o tamanho da arrays
